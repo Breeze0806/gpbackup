75c1baf6164f91bc2b029305572e05501c273895
Brent Doil <bdoil@vmware.com>
2022-01-17 18:19:00 -0500

    go_components -> go-components
    Authored-by: Brent Doil <bdoil@vmware.com>

--- ci/scripts/build-os-tars.bash
+++ ci/scripts/build-os-tars.bash
@@ -7,7 +7,7 @@ pushd gpbackup_tar
   printf "#!/bin/sh\nset -x\ntar -xzvf bin_gpbackup.tar.gz -C \$GPHOME" > install_gpdb_component
   chmod +x install_gpdb_component
 
-  tar -xzf ../gpbackup-go-components/go_components.tar.gz
+  tar -xzf ../gpbackup-go-components/go-components.tar.gz
   cp ../ddboost_components/*version .
   cp gpbackup_version version
   cp ../gpbackup-release-license/open_source_license_VMware_Tanzu_Greenplum_Backup_and_Restore*.txt open_source_licenses_VMware_Tanzu_Greenplum_Backup_and_Restore.txt
